5539e51655ac9801e1ebfead6112ea710ff716af
sri080797 <szd0151@auburn.edu>
2024-10-12 17:16:33 -0500

    Add files via upload

---
+++ Heyday.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Greeting and welcoming wishes
+greeting="Hey"
+Wishes="Welcome to Auburn's Hey Day!"
+
+# Asking name of user
+echo "Hey there! What's your name?"
+read -p "Enter your name: " name
+
+# Display the personalized greeting and  wishes for Auburn Hey day
+echo "$greeting, $name!"
+echo "$Wishes"
+echo "Have a great day, $name!"
+
