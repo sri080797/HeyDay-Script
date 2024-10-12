5bae111f13e6f1e7ea133a8a675ac28b52469c82
sri080797 <szd0151@auburn.edu>
2024-10-12 17:34:06 -0500

    Update Heyday.sh

--- Heyday.sh
+++ Heyday.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Greeting and welcoming wishes
-greeting="Hey"
+Greeting="Hey"
 Wishes="Welcome to Auburn's Hey Day!"
 
 # Asking name of user
@@ -9,7 +9,7 @@ echo "Hey there! What's your name?"
 read -p "Enter your name: " name
 
 # Display the personalized greeting and  wishes for Auburn Hey day
-echo "$greeting, $name!"
+echo "$Greeting, $name!"
 echo "$Wishes"
 echo "Have a great day, $name!"
 
